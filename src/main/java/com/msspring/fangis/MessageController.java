433022e89e26b17c2d3d3ac8e0d7b0c63f6bd041
bmorel <bmorel@student.ethz.ch>
2021-12-08 20:14:04 +0100

    now full time bean farmer

--- src/main/java/com/msspring/fangis/MessageController.java
+++ src/main/java/com/msspring/fangis/MessageController.java
@@ -1,19 +1,32 @@
 package com.msspring.fangis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.Map;
+
 @Controller
 public class MessageController {
+
+    @Autowired
+    private Map<String, String> userstatus;
 
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(@Header("simpSessionId") String sessionId, UserNameMessage message) throws Exception {
-        System.out.println(sessionId + "       ta me  \n");
+
+
+        userstatus.put(message.getName(), "fett");
+
+        for (Map.Entry entry : userstatus.entrySet()) {
+            System.out.println(entry.getKey());
+        }
+
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 
--- src/main/java/com/msspring/fangis/WebSocketConfig.java
+++ src/main/java/com/msspring/fangis/WebSocketConfig.java
@@ -1,10 +1,14 @@
 package com.msspring.fangis;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,6 +23,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+    @Bean
+    public Map<String,String> userstatus() {
+        Map<String, String> userstatusmap = new HashMap<>();
+        return userstatusmap;
     }
 
 }
