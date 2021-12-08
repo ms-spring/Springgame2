82ea9f242683472fb58237afad3652614c202cbc
bmorel <bmorel@student.ethz.ch>
2021-12-08 19:35:44 +0100

    Greeting controller can access session Id

--- src/main/java/com/msspring/fangis/GreetingController.java
+++ src/main/java/com/msspring/fangis/GreetingController.java
@@ -1,5 +1,6 @@
 package com.msspring.fangis;
 
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,8 @@ public class GreetingController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
+    public Greeting greeting(@Header("simpSessionId") String sessionId, HelloMessage message) throws Exception {
+        System.out.println(sessionId + "       ta me  \n");
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 
