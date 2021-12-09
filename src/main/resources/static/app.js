c16ed25fe31817b86ef559201db330e1b31dc719
sscholbe <sscholbe@student.ethz.ch>
2021-12-09 21:46:14 +0100

    Geet Comeet

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,3 +1,11 @@
+
+function setStage(id) {
+    $('#stage-connect').toggle(id === 'connect');
+    $('#stage-login').toggle(id === 'login');
+    $('#stage-lobby').toggle(id === 'lobby');
+    $('#stage-game').toggle(id === 'game');
+}
+
 var stompClient = null;
 
 function setConnected(connected) {
@@ -5,8 +13,7 @@ function setConnected(connected) {
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
         $("#conversation").show();
-    }
-    else {
+    } else {
         $("#conversation").hide();
     }
     $("#greetings").html("");
@@ -40,11 +47,39 @@ function showGreeting(message) {
     $("#greetings").append("<tr><td>" + message + "</td></tr>");
 }
 
+$(window).on('load', () => {
+    setStage('connect');
+})
+
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    $("#connect").click(function () {
+        connect();
+    });
+    $("#disconnect").click(function () {
+        disconnect();
+    });
+    $("#send").click(function () {
+        sendName();
+    });
+    $("#btn-connect").click(() => {
+        setStage('login');
+    });
+    $("#btn-login").click(() => {
+        setStage('lobby');
+    });
+    $("#btn-game-1").click(() => {
+        setStage('game');
+        alert("Sunny Al Savarim Game 1");
+    })
+    $("#btn-game-2").click(() => {
+        setStage('game');
+        alert("Sunny Al Savarim Game 2");
+    })
+    $("#btn-game-3").click(() => {
+        setStage('game');
+        alert("Sunny Al Savarim Game 3");
+    })
 });
