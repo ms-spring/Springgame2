eaac8ad05b16ed14df3832d6d30aa1cb8063c61a
sscholbe <sscholbe@student.ethz.ch>
2021-12-19 13:55:26 +0100

    Added working collision detection and refactoring

--- src/main/resources/static/game.js
+++ src/main/resources/static/game.js
@@ -1,16 +1,32 @@
 const MOVE_NONE = 0, MOVE_LEFT = 1, MOVE_RIGHT = 2, MOVE_UP = 4, MOVE_DOWN = 8;
 const WIDTH = 800, HEIGHT = 600;
 
-class Player {
+class Entity {
     constructor(game) {
         this.game = game;
-        this.name = "Omae Wa";
+    }
+
+    update(t) {
+
+    }
+
+    draw(ctx) {
+
+    }
+}
+
+class Player extends Entity {
+    constructor(game, name) {
+        super(game);
+        this.name = name;
         this.x = 0;
         this.y = 0;
         this.move = MOVE_NONE;
     }
 
     update(t) {
+        super.update(t);
+
         const SPEED = 200, RADIUS = 10;
 
         // Determine the movement based on flags
@@ -28,7 +44,8 @@ class Player {
         }
 
         // Check collision
-        for (let o of this.game.obstacles) {
+        for (let o of this.game.getEntitiesOfClass(Obstacle)) {
+            // Convert player coordinates relative to obstacle so that the obstacle is no longer rotated
             let lx = this.x - o.x;
             let ly = this.y - o.y;
             let r = (lx ** 2 + ly ** 2) ** 0.5;
@@ -36,21 +53,19 @@ class Player {
             lx = r * Math.cos(theta);
             ly = r * Math.sin(theta);
 
+            // Check for overlap of hit boxes
             if (lx > -o.w / 2 && lx < o.w / 2 && ly > -o.h / 2 && ly < o.h / 2) {
-                let fromLeft = lx + o.w / 2, fromRight = o.w / 2 - lx, fromTop = ly + o.h / 2,
-                    fromBottom = o.h / 2 - ly;
-                let mini = Math.min(fromLeft, fromRight, fromTop, fromBottom);
-                if (mini === fromLeft) {
-                    lx -= fromLeft;
-                } else if (mini === fromRight) {
-                    lx += fromRight;
-                } else if (mini === fromTop) {
-                    ly -= fromTop;
-                } else if (mini === fromBottom) {
-                    ly += fromBottom;
-                }
+                // Compute overlap from left, right, top and bottom
+                // Move so that the least overlap is resolved
+                let overlap = [lx + o.w / 2, o.w / 2 - lx, ly + o.h / 2, o.h / 2 - ly];
+                let min = Math.min(...overlap);
+                if (min === overlap[0]) lx -= overlap[0];
+                if (min === overlap[1]) lx += overlap[1];
+                if (min === overlap[2]) ly -= overlap[2];
+                if (min === overlap[3]) ly += overlap[3];
             }
 
+            // Convert updated relative coordinates back again to player coordinates
             r = (lx ** 2 + ly ** 2) ** 0.5;
             theta = Math.atan2(ly, lx);
             lx = r * Math.cos(theta + o.r);
@@ -63,30 +78,59 @@ class Player {
         this.x = Math.min(Math.max(this.x, RADIUS), WIDTH - RADIUS);
         this.y = Math.min(Math.max(this.y, RADIUS), HEIGHT - RADIUS);
     }
+
+    draw(ctx) {
+        super.draw(ctx);
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.fill();
+    }
 }
 
-class Obstacle {
-    constructor(x, y, w, h, r) {
+class Obstacle extends Entity {
+    constructor(game, x, y, w, h, r) {
+        super(game);
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.r = r;
     }
+
+    update(t) {
+        super.update(t);
+    }
+
+    draw(ctx) {
+        super.draw(ctx);
+        ctx.fillStyle = "black";
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.r);
+        ctx.translate(-this.x, -this.y);
+        ctx.fillRect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+        ctx.restore();
+    }
 }
 
 export class Game {
     constructor() {
-        let player1 = new Player(this);
+        let player1 = new Player(this, "Oma Wae");
         player1.x = 200;
         player1.y = 300;
-        player1.name = "Watashi";
-        let player2 = new Player(this);
+        let player2 = new Player(this, "Opa Mo");
         player2.x = 500;
         player2.y = 100;
-        player2.name = "Omae";
-        this.players = [player1, player2];
-        this.obstacles = [new Obstacle(200, 200, 100, 100, Math.PI / 4), new Obstacle(500, 400, 100, 10, 0)];
+        this.entities = [
+            player1,
+            player2,
+            new Obstacle(this, 200, 200, 100, 100, Math.PI / 4),
+            new Obstacle(this, 500, 400, 100, 10, 0)
+        ];
+        this.player = player1;
+
         this.canvas = document.querySelector("#game-canvas");
         this.ctx = this.canvas.getContext("2d");
         this.lastUpdate = null;
@@ -94,45 +138,38 @@ export class Game {
         this.draw();
 
         $(window).on('keydown', (e) => {
-            this.players[0].move |= (e.keyCode === 37) * MOVE_LEFT | (e.keyCode === 39) * MOVE_RIGHT
+            this.player.move |= (e.keyCode === 37) * MOVE_LEFT | (e.keyCode === 39) * MOVE_RIGHT
                 | (e.keyCode === 38) * MOVE_UP | (e.keyCode === 40) * MOVE_DOWN;
         });
         $(window).on('keyup', (e) => {
-            this.players[0].move &= ~((e.keyCode === 37) * MOVE_LEFT | (e.keyCode === 39) * MOVE_RIGHT
+            this.player.move &= ~((e.keyCode === 37) * MOVE_LEFT | (e.keyCode === 39) * MOVE_RIGHT
                 | (e.keyCode === 38) * MOVE_UP | (e.keyCode === 40) * MOVE_DOWN);
         });
 
+        // Initialize game loop
         window.requestAnimationFrame((t) => this.update(t));
+    }
+
+    getEntitiesOfClass(cls) {
+        return this.entities.filter(e => e instanceof cls);
     }
 
     update(t) {
         let delta = (t - this.lastUpdate) / 1000;
-        for (let p of this.players) {
-            p.update(delta);
+        for (let e of this.entities) {
+            e.update(delta);
         }
         this.draw();
-        window.requestAnimationFrame((t) => this.update(t));
+
+        // Setup next game loop update
         this.lastUpdate = t;
+        window.requestAnimationFrame((t) => this.update(t));
     }
 
     draw() {
-        let g = this.ctx;
-        g.clearRect(0, 0, 800, 600);
-
-        for (let o of this.obstacles) {
-            g.save();
-            g.translate(o.x, o.y);
-            g.rotate(o.r);
-            g.translate(-o.x, -o.y);
-            g.fillRect(o.x - o.w / 2, o.y - o.h / 2, o.w, o.h);
-            g.restore();
-        }
-
-        for (let p of this.players) {
-            g.beginPath();
-            g.arc(p.x, p.y, 10, 0, 2 * Math.PI);
-            g.closePath();
-            g.fill();
+        this.ctx.clearRect(0, 0, 800, 600);
+        for (let e of this.entities) {
+            e.draw(this.ctx);
         }
     }
 }
